95d9f0c27bb74b5d76730ef9c3557bbe19490409
Geordie King <geordie.king@gmail.com>
2022-06-12 21:19:01 +0100

    added start-up structure

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 
 const PORT = 8005
 
+//Databse setup
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'myanwolfe-api-thing'
@@ -16,10 +17,16 @@ MongoClient.connect(dbConnectionString)
         console.log(`Connected to the ${dbName} database`)
     })
 
+ //Tells our App how to do it's things -- middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 
-    
+
+// Port set up
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
