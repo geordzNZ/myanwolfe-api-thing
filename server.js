b91bc305f1700d47b1cf0d72876f4d00e448af9d
Geordie King <geordie.king@gmail.com>
2022-06-12 21:01:45 +0100

    connected to db

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
-constMongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
-const PORT = 1929
+const PORT = 8005
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'myanwolfe-api-thing'
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to MongoDB')
+        db = client.db(dbName)
+        console.log(`Connected to the ${dbName} database`)
+    })
+
+
+
+
+    
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
